9f9bb424055a6e47ef0dffd0f488804fd2219860
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-09-13 13:29:01 +0200

    Add 'Mockito' library
    User documentation: https://site.mockito.org/
    Code: https://github.com/mockito/mockito

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,19 +1,22 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("A foo")
+@DisplayName("Foo")
 class FooTest {
 
-  private Foo foo = new Foo();
+  private Bar bar = mock(Bar.class);
+
+  private Foo foo = new Foo(bar);
 
   @Test
-  @DisplayName("returns foo")
-  void isNotEmpty() {
-    assertThat(foo.foo())
-      .isEqualTo("foo");
+  @DisplayName("returns mocked bar")
+  void returns_mocked_bar() {
+    when(bar.bar()).thenReturn("foo");
+    assertThat(foo.foo("plop")).isEqualTo("foo");
   }
-
 
 }
