ee17659defea7da28ce0644f4fd4a3c570564a97
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-09-13 13:30:25 +0200

    Add a parameterized test

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -4,6 +4,8 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Foo")
 class FooTest {
@@ -17,6 +19,14 @@ class FooTest {
   void returns_mocked_bar() {
     when(bar.bar()).thenReturn("foo");
     assertThat(foo.foo("plop")).isEqualTo("foo");
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"foo", "bar", "baz"})
+  @DisplayName("returns mocked bar for")
+  void foo_bar_baz_return_mocked_bar(String input) {
+    when(bar.bar()).thenReturn("foo");
+    assertThat(foo.foo(input)).isEqualTo("foo");
   }
 
 }
