23b55b77416ac701a1bc74eca2bc19607305aa48
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-09-13 13:32:16 +0200

    Add 'jqwik' library for property-based testing
    User Guide: https://jqwik.net/
    Code: https://github.com/jlink/jqwik

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,3 +1,10 @@
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Label;
+import net.jqwik.api.Property;
+import net.jqwik.api.Provide;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -8,6 +15,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Foo")
+@Label("Foo")
 class FooTest {
 
   private Bar bar = mock(Bar.class);
@@ -27,6 +35,17 @@ class FooTest {
   void foo_bar_baz_return_mocked_bar(String input) {
     when(bar.bar()).thenReturn("foo");
     assertThat(foo.foo(input)).isEqualTo("foo");
+  }
+
+  @Property
+  @Label("always returns bar")
+  boolean always_return_bar(@ForAll("anyString") String anyString) {
+    return new Foo(new Bar()).foo(anyString).equals("bar");
+  }
+
+  @Provide
+  Arbitrary<String> anyString() {
+    return Arbitraries.strings().all();
   }
 
 }
