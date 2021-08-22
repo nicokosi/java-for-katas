af50f44783ce41cb9bf024ddc536113d6e47754a
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-08-22 18:50:53 +0200

    Configure useLatestVersionsCheck for stable dependencies
    https://github.com/ben-manes/gradle-versions-plugin#rejectversionsif-and-componentselection

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,3 +43,16 @@ dependencies {
 tasks.withType<Test> {
   useJUnitPlatform()
 }
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+  rejectVersionIf {
+    isNonStable(candidate.version)
+  }
+}
