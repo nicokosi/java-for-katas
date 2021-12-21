1046ac8917f4f9e017663421267fc1db1b6dd381
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-12-21 07:32:34 +0100

    Fix test warnings and segregate Property-based Test 🔕

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,18 +1,12 @@
-import net.jqwik.api.Arbitraries;
-import net.jqwik.api.Arbitrary;
-import net.jqwik.api.ForAll;
 import net.jqwik.api.Label;
-import net.jqwik.api.Property;
-import net.jqwik.api.Provide;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @DisplayName("Foo")
 @Label("Foo")
@@ -34,17 +28,6 @@ class FooTest {
   void foo_bar_baz_return_mocked_bar(String input) {
     when(bar.bar()).thenReturn("foo");
     assertThat(foo.foo(input)).isEqualTo("foo");
-  }
-
-  @Property
-  @Label("always returns bar")
-  boolean always_return_bar(@ForAll("anyString") String anyString) {
-    return new Foo(new Bar()).foo(anyString).equals("bar");
-  }
-
-  @Provide
-  Arbitrary<String> anyString() {
-    return Arbitraries.strings().all();
   }
 
 }
