6dd46a7d6aa0885c0d3302112b5319baf674fc09
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2022-06-10 05:42:09 +0200

    Parameterized tests

--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,42 +10,34 @@ class LeapYearTest {
   @Nested
   class LeapYears {
     @DisplayName("All years divisible by 400 ARE leap years")
-    @Test
-    void div400() {
-      assertThat(LeapYear.leapYear(400)).isTrue();
-      assertThat(LeapYear.leapYear(800)).isTrue();
-      assertThat(LeapYear.leapYear(1200)).isTrue();
-      assertThat(LeapYear.leapYear(1600)).isTrue();
-      assertThat(LeapYear.leapYear(2000)).isTrue();
+    @ParameterizedTest
+    @ValueSource(ints = {400, 800, 1200, 1600, 2000})
+    void div400(int year) {
+      assertThat(LeapYear.leapYear(year)).isTrue();
     }
 
     @DisplayName("All years divisible by 4 but not by 100 ARE leap years")
-    @Test
-    void div4Not100() {
-      assertThat(LeapYear.leapYear(4)).isTrue();
-      assertThat(LeapYear.leapYear(8)).isTrue();
-      assertThat(LeapYear.leapYear(12)).isTrue();
+    @ParameterizedTest
+    @ValueSource(ints = {4, 8, 12})
+    void div4Not100(int year) {
+      assertThat(LeapYear.leapYear(year)).isTrue();
     }
   }
 
   @Nested
   class NotLeapYears {
     @DisplayName("All years divisible by 100 but not by 400 are NOT leap years")
-    @Test
-    void div100Not400() {
-      assertThat(LeapYear.leapYear(100)).isFalse();
-      assertThat(LeapYear.leapYear(200)).isFalse();
-      assertThat(LeapYear.leapYear(300)).isFalse();
-      assertThat(LeapYear.leapYear(500)).isFalse();
+    @ParameterizedTest
+    @ValueSource(ints = {100, 200, 300, 500})
+    void div100Not400(int year) {
+      assertThat(LeapYear.leapYear(year)).isFalse();
     }
 
     @DisplayName("All years not divisible by 4 are NOT leap years")
-    @Test
-    void notDiv4() {
-      assertThat(LeapYear.leapYear(1)).isFalse();
-      assertThat(LeapYear.leapYear(2)).isFalse();
-      assertThat(LeapYear.leapYear(3)).isFalse();
-      assertThat(LeapYear.leapYear(5)).isFalse();
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 5})
+    void notDiv4(int year) {
+      assertThat(LeapYear.leapYear(year)).isFalse();
     }
   }
 
