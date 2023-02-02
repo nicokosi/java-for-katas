85e6a4f734dd45f9c25c19d8dd5e23614e2dd123
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2023-02-02 05:17:20 +0100

    Reformat tests

--- src/test/java/fraction/FractionTest.java
+++ src/test/java/fraction/FractionTest.java
@@ -10,7 +10,9 @@ class FractionTest {
   @Test
   void same_denominator() {
     assertThat(
-      new Fraction(1, 1).add(new Fraction(2, 1)))
+      new Fraction(1, 1)
+        .add(
+          new Fraction(2, 1)))
       .isEqualTo(
         new Fraction(3, 1)
       );
