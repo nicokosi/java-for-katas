7a0be2cc99be4ca7089d1b95355e671c5989ae81
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2023-02-02 05:14:49 +0100

    Refactor

--- src/main/java/fraction/Fraction.java
+++ src/main/java/fraction/Fraction.java
@@ -1,6 +1,6 @@
 package fraction;
 
-import static java.lang.Integer.max;
+import static java.lang.Math.max;
 
 public record Fraction(int numerator, int denominator) {
   public Fraction add(Fraction fraction) {
@@ -9,13 +9,10 @@ public record Fraction(int numerator, int denominator) {
         this.numerator + fraction.numerator,
         fraction.denominator);
     } else {
-      if (this.denominator > fraction.denominator) {
-        var numerator = this.numerator + fraction.numerator * this.denominator / fraction.denominator;
-        return new Fraction(numerator, this.denominator);
-      } else {
-        var numerator = fraction.numerator + this.numerator * fraction.denominator / this.denominator;
-        return new Fraction(numerator, fraction.denominator);
-      }
+      var numerator = this.denominator > fraction.denominator ?
+        this.numerator + fraction.numerator * this.denominator / fraction.denominator :
+        fraction.numerator + this.numerator * fraction.denominator / this.denominator;
+      return new Fraction(numerator, max(this.denominator, fraction.denominator));
     }
   }
 }
