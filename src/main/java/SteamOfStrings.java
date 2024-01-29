8de698dc972ae5548459ff3bd158150be2d2a947
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2024-01-29 06:16:53 +0100

    Cleaner syntax

--- src/main/java/SteamOfStrings.java
+++ src/main/java/SteamOfStrings.java
@@ -3,11 +3,12 @@ import java.util.stream.Stream;
 public class SteamOfStrings {
 
   public static void main(String[] args) {
-    Stream.Builder<String> streamOfStrings = Stream.builder();
+    var streamOfStrings = Stream.builder();
     streamOfStrings.add("hello");
     streamOfStrings.add("world");
     streamOfStrings.add("👋");
-    streamOfStrings.build().forEach(System.out::println);
+    streamOfStrings.build()
+      .forEach(System.out::println);
   }
 
 }
